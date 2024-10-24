c63c6dbd320a62baac86e52aa3d3327e01285160
PipeFajardo <felipefajardoquintero@gmail.com>
2024-10-23 20:56:50 -0500

    changing the bullet's color to a brighter one

--- settings.py
+++ settings.py
@@ -12,8 +12,8 @@ class Settings:
         self.ship_speed = 3
 
         # Bullets settings
-        self.bullet_speed = 2.0
+        self.bullet_speed = 2.5
         self.bullet_width = 3
         self.bullet_height = 15
-        self.bullet_color = (60, 60, 60)
-        self.bullets_allowed = 3
+        self.bullet_color = (228, 57, 57)
+        self.bullets_allowed = 5
