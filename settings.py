21cf223661ed2e70eedffc6548d0f94c7642b864
PipeFajardo <felipefajardoquintero@gmail.com>
2025-02-26 16:32:06 -0500

    Displaying highest score

--- settings.py
+++ settings.py
@@ -46,7 +46,6 @@ class Settings:
         self.alien_speed *= self.speedup_scale
 
         self.alien_points = int(self.alien_points * self.score_scale)
-        print (self.alien_points)
         
     def set_difficulty(self, difficulty):
         """Adjust game difficulty settings based on selection."""
